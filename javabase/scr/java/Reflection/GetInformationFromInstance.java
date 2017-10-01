641de832050f216956d6933abae2eb69418666ca
kincolle <kincolle@gmail.com>
2017-10-01 18:15:28 +0800

    add codes about reflection drill

--- javabase/scr/java/Reflection/GetInformationFromInstance.java
+++ javabase/scr/java/Reflection/GetInformationFromInstance.java
@@ -1,19 +1,26 @@
 package Reflection;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
-public class Reflection {
+public class GetInformationFromInstance {
     public static void main(String[] args) throws NoSuchMethodException, ClassNotFoundException {
         TestClass r = new TestClass();
         Class<?> c1 = Class.forName(TestClass.class.getName());
         while (c1!=null) {
-
+            System.out.println("---this is method---");
             for(Method m : c1.getDeclaredMethods()){
                 System.out.println(Modifier.toString(m.getModifiers())+ " " +
                         m.getReturnType().getCanonicalName()
                         +" "+m.getName()+ Arrays.toString(m.getParameters())
+                );
+            }
+            System.out.println("this is fields");
+            for(Field m : c1.getDeclaredFields()){
+                System.out.println(Modifier.toString(m.getModifiers()) + " "
+                        +m.getName()
                 );
             }
             c1=c1.getSuperclass();
---
+++ javabase/scr/java/Reflection/GetValue.java
@@ -0,0 +1,20 @@
+package Reflection;
+
+import java.lang.reflect.Field;
+
+public class GetValue {
+    public static void main(String[] args) throws NoSuchMethodException, ClassNotFoundException, IllegalAccessException {
+        TestClassEx t = new TestClassEx();
+
+        for (Field f : t.getClass().getDeclaredFields()) {
+            f.setAccessible(true);
+            Object value = f.get(t);
+            System.out.println(f.getName() + ":" + value);
+        }
+
+    }
+}
+
+class TestClassEx{
+    private String kincolle = "This is kincolle\'s reflection drill test.";
+}
