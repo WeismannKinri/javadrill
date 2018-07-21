58fafb6d2e87f3f0f352e437d00047e35fce1e1c
chengjin <chengjin@Ctrip.com>
2018-07-21 12:58:27 +0800

    add code

--- spring/springAnnotation/src/java/first/Aop.java
+++ spring/springAnnotation/src/java/first/Aop.java
@@ -1,5 +1,15 @@
 package first;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
 
+@Aspect
 public class Aop {
+
+    @After("")
+    public void after(){
+        System.out.println("after");
+    }
 }
+
+
