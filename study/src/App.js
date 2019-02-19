e381098ed3a526736b41d761dfc24e47c03f06d7
chengjin <chengjin@Ctrip.com>
2019-02-19 15:18:17 +0800

    add antd code

--- study/src/App.js
+++ study/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,11 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
+          <div className="App">
+            <Button type="primary">Button</Button>
+          </div>
+
           <a
             className="App-link"
             href="https://reactjs.org"
